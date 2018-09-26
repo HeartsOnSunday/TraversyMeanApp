c587771b4a91ff78b04b1b78dd6d5842d2e0aadf
HeartsOnSunday <leahgdg@hotmail.com>
2018-09-26 17:52:01 -0400

    validate http get and post req

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,6 +45,31 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route GET api/users/login
+// @desc Login user / Returns JWT Token (JSON Web Token)
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    //check for user
+    if (!user) {
+      return res.status(404).json({ email: "User email not found" });
+    }
+
+    //Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password is incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
 
 //routes/api/users is implied by the route
