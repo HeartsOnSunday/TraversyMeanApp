adaa09e16c6c5f36d5fdcce52a85ffac72a5d0fd
HeartsOnSunday <leahgdg@hotmail.com>
2018-09-27 14:49:33 -0400

    isEmpty() sends back errors w/ Validator Module

--- validation/register.js
+++ validation/register.js
@@ -1,5 +1,15 @@
 const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+  //password must be longer that two characters
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+    //errors is an object but we want to use a string for Validator Module
+  };
 };
