e3f336255d5667940a40ac4d848db847bf96693b
HeartsOnSunday <leahgdg@hotmail.com>
2018-10-08 19:32:04 -0400

    errors incorrectly rendering for filled fields

--- validation/experience.js
+++ validation/experience.js
@@ -17,7 +17,7 @@ module.exports = function validateExperienceInput(data) {
   }
 
   if (Validator.isEmpty(data.from)) {
-    errors.from = "From field is required";
+    errors.from = "From date field is required";
   }
 
   ///// 7:12 video 022!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
