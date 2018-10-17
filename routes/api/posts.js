a345b7afc891cdc38e2fb5becf17f58ee41bf3ed
HeartsOnSunday <leahgdg@hotmail.com>
2018-10-16 20:35:41 -0400

    delete route success

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,6 +16,37 @@ const validatePostInput = require("../../validation/post");
 // @access Public  --HEADERINFO
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route GET api/posts
+// @desc Get posts
+// @access Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopostsfound: "There were no posts found" })
+    );
+});
+
+// @route GET api/posts/:id
+// @desc Get posts by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.json(post);
+      } else {
+        res
+          .status(404)
+          .json({ nopostsfound: "No posts were found with that ID" });
+      }
+    })
+    .catch(err =>
+      res.status(404).json({ nopostsfound: "No post were found with that ID" })
+    );
+});
+
 // @route POST api/posts/ --HEADERINFO
 // @desc CREATE post --HEADERINFO
 // @access PRIVATE  --HEADERINFO
@@ -40,6 +71,30 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route DELETE api/posts/:id
+// @desc DELETE posts by id number
+// @access PRIVATE
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // verify post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "Unauthorized Action" });
+          }
+          // Delete Post
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
   }
 );
 
