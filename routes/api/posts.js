dc8ad396e83ede45fec77a73b89fa1976098eaa9
HeartsOnSunday <leahgdg@hotmail.com>
2018-10-23 20:24:09 -0400

    fix error for toString()

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -98,35 +98,6 @@ router.delete(
   }
 );
 
-// @route POST api/posts/like/:id
-// @desc Like Post
-// @access PRIVATE
-/*router.post(
-  "/like/:id",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Profile.findOne({ user: req.user.id }).then(profile => {
-      Post.findById(req.params.id)
-        .then(post => {
-          // verify post owner
-          if (
-            post.likes.filter(like => like.user.toString() === req.user.id)
-              .length > 0
-          ) {
-            return res
-              .status(400)
-              .json({ alreadyliked: "User already liked this post" });
-          }
-          // Add user id to likes array w/ unshift (.push() is another option)
-          post.likes.unshift({ user: req.user.id });
-          post.save().then(post => res.json(post));
-        })
-        .catch((err = res.status(404).json({ postnotfound: "No post found" })));
-    });
-  }
-);
-*/
-
 // @route   POST api/posts/like/:id
 // @desc    Like post
 // @access  Private
@@ -181,7 +152,7 @@ router.post(
             .indexOf(req.user.id);
 
           //splice out of array
-          post.lokes.splice(removeIndex, 1);
+          post.likes.splice(removeIndex, 1);
 
           // Save
           post.save().then(post => res.json(post));
