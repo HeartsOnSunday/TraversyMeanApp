395d66415f4515184f0c4b45e2f020597896c051
HeartsOnSunday <leahgdg@hotmail.com>
2018-11-03 00:35:52 -0400

    included Nav/Foot/Landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Landing from "./components/layouts/Landing";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>MY REACT APP</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
